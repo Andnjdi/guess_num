2d59b45327cedfe21838bd23b06300e2654a95a8
Andnjdi <antonbolbat2001@gmail.com>
2026-03-18 21:59:31 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("\t\t\t\tGame 'GUESS NUMBER'");
+        int hidden_num = rand.nextInt(100) + 1;
+        System.out.println("*test" + hidden_num);
+        int attemps = 0;
+        while (true) {
+            System.out.print("Guess num (1-100): ");
+            attemps++;
+            int guess = scan.nextInt();
+            if (guess == 0) {
+                System.out.println("You're lose!");
+                break;
+            }
+            else if (guess < 0 || guess > 100) {
+                System.out.println("Try again");
+                attemps--;
+                continue;
+            }
+            else if (guess == hidden_num) {
+                System.out.println("You're win!");
+                System.out.println("Amount attemps: " + attemps);
+                break;
+            }
+            else if (guess > hidden_num) {
+                System.out.println("My number less");
+            }
+            else if (guess < hidden_num) {
+                System.out.println("My number more");
+            }
+        }
+    }
+}
